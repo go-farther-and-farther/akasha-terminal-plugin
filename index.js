83fd082d6ab86241cbf0b7954a22ad5ceb1f937f
越追越远 <2859167710@qq.com>
2022-11-12 06:25:46 +0000

    push
    Signed-off-by: 越追越远 <2859167710@qq.com>

---
+++ index.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs'
+import Ver from './components/Version.js'
+import chalk from 'chalk'
+
+const files = fs.readdirSync('./plugins/akasha-terminal-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info(chalk.green('------^-^------'))
+logger.info(chalk.yellow(`虚空插件${Ver.ver}初始化~`))
+logger.info(chalk.green('---------------'))
+
+if (!await redis.get(`akasha:notice:deltime`)) {
+    await redis.set(`akasha:notice:deltime`, "600")
+}
+
+
+files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+export { apps }
