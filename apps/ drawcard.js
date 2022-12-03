668a32c5b37713500c78aa142148be187a9b878e
go-farther-and-farther <2859167710@qq.com>
2022-12-03 18:52:48 +0800

    modified:   apps/ drawcard.js

--- apps/ drawcard.js
+++ apps/ drawcard.js
@@ -8,6 +8,10 @@ let Template = {//创建该用户
 };
 let exerciseCD = {};
 let Cool_time = 5;
+var weapon = JSON.parse(fs.readFileSync(dirpath2, "utf8"));
+let num3 = weapon[`3星数量`]
+let num4 = weapon[`4星数量`]
+let num5 = weapon[`5星数量`]
 export class drawcard extends plugin {
     constructor() {
         super({
@@ -31,6 +35,18 @@ export class drawcard extends plugin {
                     reg: "^#(决斗|虚空|抽卡)?(抽武器|祈愿)$", //匹配消息正则，命令正则
                     /** 执行方法 */
                     fnc: 'weapon'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#武器库$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'weaponWarehouse'
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: "^#我的武器$", //匹配消息正则，命令正则
+                    /** 执行方法 */
+                    fnc: 'myweapon'
                 }
             ]
         })
@@ -39,6 +55,13 @@ export class drawcard extends plugin {
      * 
      * @param e oicq传递的事件参数e
      */
+    async weaponWarehouse(e) {
+        var weapon = JSON.parse(fs.readFileSync(dirpath2, "utf8"));
+        let num3 = weapon[`3星数量`]
+        let num4 = weapon[`4星数量`]
+        let num5 = weapon[`5星数量`]
+        e.reply(`武器库总量三星${num3}四星${num4}五星${num5}`)
+    }
     async signin(e) {
         let user_id = e.user_id;
         let filename = `${user_id}.json`;
@@ -88,15 +111,13 @@ export class drawcard extends plugin {
             e.reply(`好可惜，但是你没有纠缠之缘了！`);
             return;
         }
-        if (user_id == '2859167710') { json['money']++ }
+        if (user_id == '2859167710') { json['money']++ }//开发者开挂
         else { json['money']-- }
+        //获取随机数，判断武器等级
         let Grade = Math.floor(1000 * Math.random())
         if (Grade < 16) { Grade = 5 }
         else if (Grade < 150) { Grade = 4 }
         else { Grade = 3 }
-        let num3 = weapon[`3星数量`]
-        let num4 = weapon[`4星数量`]
-        let num5 = weapon[`5星数量`]
         if (Grade == 5)
             var num = Math.floor(1 + num5 * Math.random())
         else if (Grade == 4)
@@ -105,19 +126,42 @@ export class drawcard extends plugin {
             var num = Math.floor(1 + num3 * Math.random())
         let name = weapon[Grade][num];
         if (!json.hasOwnProperty(Grade)) {//如果json中不存在该用户
-            json[Grade] = { "name": 1 }
+            json[Grade] = { num: 1 }//数量1
         }
-        if (!json[Grade].hasOwnProperty(num)){
-                json[Grade][num] = 1
+        if (!json[Grade].hasOwnProperty(num)) {
+            json[Grade][num] = 1
         }
-        else{
-                json[Grade][num]++
+        else {
+            json[Grade][num]++
         }
-        //(内测：武器库总量三星${num3}四星${num4}五星${num5}，武器${Grade}星${num}号)
-        let msg = [`你已经有${json[Grade][num]}把${name}了(内测：武器库总量三星${num3}四星${num4}五星${num5}，武器${Grade}星${num}号),你还有${json['money']}纠缠之缘`,
+        let msg = [`你已经有${json[Grade][num]}把${name}了,你还有${json['money']}纠缠之缘`,
         segment.image(`plugins/akasha-terminal-plugin/resources/weapon/${Grade}/${name}.png`)]
         e.reply(msg)
         fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
+        return
+    }
+    async myweapon(e) {
+        let user_id = e.user_id;
+        let filename = `${user_id}.json`;
+        //判断冷却
+        //如果文件不存在，创建文件
+        if (!fs.existsSync(dirpath + "/" + filename)) {
+            e.reply('你还没有注册呢，请使用 #虚空签到 注册')
+            return
+        }
+        //读取文件
+        var json = JSON.parse(fs.readFileSync(dirpath + "/" + filename, "utf8"));
+        var weapon = JSON.parse(fs.readFileSync(dirpath2, "utf8"));
+
+
+        let msg = `三星武器：`;
+        if (!json.hasOwnProperty(3)) {
+            for (i in json[3]) {
+                msg = msg + weapon[Grade][i]
+            }
+        }
+
+        e.reply(msg)
         return
     }
 }
