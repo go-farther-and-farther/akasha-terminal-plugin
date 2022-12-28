6ac69b628c4833cd2d08ae0a0cd9f457da6adf6a
go-farther-and-farther <2859167710@qq.com>
2022-12-28 23:36:29 +0800

    modified:   apps/getwife.js

--- apps/getwife.js
+++ apps/getwife.js
@@ -111,7 +111,7 @@ export class qqy extends plugin {
             },
             {
                 /** 命令正则匹配 */
-                reg: '^#?群cp$', //抱抱
+                reg: '^#?(群cp|cp列表)$', //抱抱
                 /** 执行方法 */
                 fnc: 'cp'
             }
@@ -579,17 +579,19 @@ export class qqy extends plugin {
         return true;
     }
     async cp(e) {//查看所有cp
-        e.reply('如果你看到这个，说明现在还在测试')
+
         var json = JSON.parse(fs.readFileSync(dirpath + "/" + filename, "utf8"));//读取文件
         let msg = `群全部cp:\n`
         let memberMap = await e.group.getMemberMap();
         let arrMember = Array.from(memberMap.values());
-        var idlist = arrMember.filter(item => {
-            return item.user_id
-        })
+        let idlist = []
+        for (i of arrMember.length) {
+            idlist[i] = arrMember[i].user_id
+        }
+        e.reply('如果你看到这个，说明现在还在测试')
         for (let i of Object.keys(json)) {
             if (idlist.includes(json[i].s))
-                msg = msg + `${json[i].s}和${i}`
+                msg = msg + `${i}和他的老婆${json[i].s}\n`
         }
         e.reply(msg)
         return true;
