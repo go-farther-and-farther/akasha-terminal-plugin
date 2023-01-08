c9e0dbb3ada52fef24c5d27b24d90868cd785596
tyg211375 <2113752439@qq.com>
2023-01-08 17:51:57 +0800

    fix

--- apps/getwife.js
+++ apps/getwife.js
@@ -5,7 +5,7 @@ import { segment } from "oicq";
 import moment from "moment"
 import command from '../components/command.js'
 import akasha_data from '../components/akasha_data.js'
-const giftpath = `plugins/akasha-terminal-plugin/resources/qylp/giftthing.homejson`
+const giftpath = `plugins/akasha-terminal-plugin/resources/qylp/giftthing.json`
 const currentTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
 let cdTime = Number(await command.getConfig("wife_cfg", "sjcd")) * 60;//随机娶群友冷却
 let cdTime2 = Number(await command.getConfig("wife_cfg", "qqcd")) * 60;//强娶冷却
