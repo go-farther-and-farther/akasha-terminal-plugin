4b93c61cc0e7374781e770bec37c5e077881ec9f
tyg211375 <2113752439@qq.com>
2023-01-15 18:02:49 +0800

    test

---
+++ apps/akashaschedule.js
@@ -0,0 +1,73 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import schedule from "node-schedule";
+import fs from 'fs'
+let redblueball_time = String(Math.floor(Math.random() * 60)) + ' ' + String(Math.floor(Math.random() * 60)) + ' ' + String(Math.floor(Math.random() * 2) + 6) + ' * * *'
+export class thumbUp extends plugin {
+	constructor() {
+		super({
+			/** 功能名称 */
+			name: '虚空抽奖',
+			/** 功能描述 */
+			dsc: '定时生成号码',
+			/** https://oicqjs.github.io/oicq/#events */
+			event: 'message',
+			/** 优先级，数字越小等级越高 */
+			priority: 1000,
+			rule: [
+				{
+					/** 命令正则匹 配 */
+					reg: "^#?双色球开奖$", //匹配消息正则，命令正则
+					/** 执行方法 */
+					fnc: 'redblueball'
+				}
+			]
+		})
+	}
+	/**
+	 * 
+	 * @param e oicq传递的事件参数e
+	 */
+    async redblueball(e){
+        if(e.isMaster)
+        redblueball_start(e);
+    }
+}
+//这个是获取一个6~7点的时间，到了时间则执行任务
+schedule.scheduleJob(redblueball_time, function () {
+	redblueball_start();
+}
+);
+
+async function redblueball_start(e) {
+    const lotterypath = `plugins/akasha-terminal-plugin/resources/qylp`
+    let filename = `lottery.json`
+    if (!fs.existsSync(lotterypath + "/" + filename)) {//如果文件不存在
+        fs.writeFileSync(lotterypath + "/" + filename, JSON.stringify({//创建文件
+        }));
+    }
+    var redballarr = []
+    for(;;){
+        let redballnum = Math.round(Math.random()*32 + 1)//生成1到33随机数
+        if(redballarr.includes(redballnum))
+            redballarr = []//含有重复数字则重新开始
+        if(redballnum<10){
+            redballnum = "0" + redballnum//小于10改为01,02形式
+        }
+        redballarr.push(redballnum)
+        if(redballarr.length == 6){
+            var ssqjson = JSON.parse(fs.readFileSync(lotterypath + "/" + filename, "utf8"));//读取文件
+            let blueballnum =  Math.round(Math.random()*15 + 1)
+            if(blueballnum<10)
+            blueballnum = "0" + blueballnum
+            let ssqdata = {
+                redball: redballarr,
+                blueball: blueballnum
+            }
+            let title = "RBB"
+            ssqjson[title] = ssqdata
+            fs.writeFileSync(lotterypath + "/" + filename, JSON.stringify(ssqjson, null, "\t"));//写入文件
+            e.reply(`本次号码为红${redballarr.toString()},蓝${blueballnum}`);
+            break//有六个不同数字时结束
+        }
+    }
+}
