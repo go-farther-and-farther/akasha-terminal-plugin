5d4b50aea977bc270ebbcaf67a469632c04cf631
tyg211375 <2113752439@qq.com>
2023-01-16 20:47:59 +0800

    test

--- apps/getwife.js
+++ apps/getwife.js
@@ -876,11 +876,11 @@ export class qqy extends plugin {
     }
     //买双色球
     async lottery1(e){
-        let ForTbuy = await redis.get(`akasha:wife-lottery1:${e.group_id}:${e.user_id}:*`);
-        if (ForTbuy) {
+        let myRBB = await redis.keys(`akasha:wife-lottery1:${e.group_id}:${e.user_id}:*`, (err, data) => { });
+        if (myRBB) {
             e.reply([
                 segment.at(e.user_id), "\n",
-                `你已经买过了`
+                `你买过了`
             ])
             return
         }
