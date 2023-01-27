3178af7429069511a5dc0128b02f613cbcae7bb8
tyg211375 <2113752439@qq.com>
2023-01-27 20:17:18 +0800

    fix

--- apps/akashaschedule.js
+++ apps/akashaschedule.js
@@ -48,13 +48,15 @@ schedule.scheduleJob('0 0 * * * *', async() => {
 );
 
 async function redblueball_start() {
-    try{
-        console.log(`正在通知群聊${Group}双色球开奖`)
-        for(let key of Group){
+    for(let key of Group){
+        try{
             await Bot.pickGroup(key).sendMsg(`娶群友小游戏双色球已开奖,今日购买的玩家们可以发送'双色球兑换'进行兑换\n也可'我的彩票'查看自己的号码`)
+            console.log(`正在通知群聊${key}双色球开奖`)
+        }
+        catch(err){
+            console.log(`群聊${key}不存在`)
         }
     }
-    catch{}
     for(let mat of yzcfg.masterQQ){
     await common.relpyPrivate(mat, `娶群友小游戏双色球已开奖,快去通知玩家们吧\n数据在本插件resources/qylp/lottery.json`)
     }
