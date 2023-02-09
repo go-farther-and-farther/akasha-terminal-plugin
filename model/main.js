088479e40ec5e27d15d15935fce57892ba82b614
go-farther-and-farther <2859167710@qq.com>
2023-02-09 17:47:11 +0800

    update

--- model/main.js
+++ model/main.js
@@ -1,6 +1,6 @@
 import path from 'path'
 /* 自定义全局插件名*/
-export const appname = 'alemon-plugin'
+export const appname = 'akasha-terminal-plugin'
 /* 自动匹配插件绝对路径 */
 export const __dirname = `${path.resolve()}${path.sep}plugins${path.sep}${appname}`
 /** 打印插件名*/
